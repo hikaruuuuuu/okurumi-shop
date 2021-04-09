fc5367e694250ecb9973d69da35277cdf9f13f22
Hikaru Sakamoto <hikkayn1202@gmail.com>
2021-04-09 17:17:46 +0900

    複数枚画像/flash

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,11 +10,12 @@ class ItemsController < ApplicationController
 
   def create
     Item.create(item_params)
+    redirect_to root_path, notice: '商品登録が完了しました'
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :image)
+    params.require(:item).permit(:name, :price, :description, images: [])
   end
 end

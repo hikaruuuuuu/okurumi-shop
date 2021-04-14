5e9db5cfc1dcf5690db40fb06743ce9c9d8a6576
Hikaru Sakamoto <hikkayn1202@gmail.com>
2021-04-14 16:19:06 +0900

    住所登録

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,12 @@ class OrdersController < ApplicationController
   end
 
   def create
+    UserInfo.create(user_info_params)
+  end
+
+  private
+
+  def  user_info_params
+    params.require(:order).permit(:first_name, :last_name, :post_code, :prefecture_id, :address, :building_number, :building_name, :phone_number)
   end
 end

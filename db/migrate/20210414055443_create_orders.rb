580030520cf6ec0332e83e001b6d3319dc8d4da2
Hikaru Sakamoto <hikkayn1202@gmail.com>
2021-04-14 15:08:24 +0900

    orders table active_hash

---
+++ db/migrate/20210414055443_create_orders.rb
@@ -0,0 +1,9 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.references :user, foreignkey: true
+      t.references :item, foreignkey: true
+      t.timestamps
+    end
+  end
+end
